e7711746caec0b897309026178089fab56800e55
= <=>
2020-06-04 20:33:34 -0500

    Solve replace with alphabet position kata

---
+++ replace_with_alphabet_position.rb
@@ -0,0 +1,29 @@
+# In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+# If anything in the text isn't a letter, ignore it and don't return it.
+# "a" = 1, "b" = 2, etc.
+
+# Example
+# alphabet_position("The sunset sets at twelve o' clock.")
+# Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+
+require 'minitest/autorun'
+
+def alphabet_position(text)
+  alphabet = {'a' => '1 ', 'b' => '2 ', 'c' => '3 ', 'd' => '4 ', 'e' => '5 ', 'f' => '6 ', 
+              'g' => '7 ', 'h' => '8 ', 'i' => '9 ', 'j' => '10 ', 'k' => '11 ', 'l' => '12 ', 
+              'm' => '13 ', 'n' => '14 ', 'o' => '15 ', 'p' => '16 ', 'q' => '17 ', 'r' => '18 ', 
+              's' => '19 ', 't' => '20 ', 'u' => '21 ', 'v' => '22 ', 'w' => '23 ', 'x' => '24 ', 
+              'y' => '25 ', 'z '=> '26 '
+             }
+  text.downcase.gsub(/\W/, '').gsub(/[a-zA-z]/, alphabet).chop  
+end
+
+
+class TestMethod < Minitest::Test
+
+  def test_alphabet_position
+    assert_equal alphabet_position("The sunset sets at twelve o' clock."), '20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11'
+    assert_equal alphabet_position("-.-'"), ''
+  end
+  
+end
